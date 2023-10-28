e272615b813fb838eb47869d5065eb784ddee5cc
jayavarsan <72146301+jayavarsan@users.noreply.github.com>
2023-10-28 10:29:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Web2Soft
+WebToSoft converts your website to a native desktop application based on the Electron framework. Without learning any programming language, you can build your macOS, Windows and Linux application within a few minutes.
+
+We have a team of four Qubots made this using electron module 
+
+
+
+# Documentation
+Link : https://prasanth-1.gitbook.io/web2soft-docs
---
+++ config.js
@@ -0,0 +1,31 @@
+// Web2Soft v2.0.0
+// Prasanth
+// kishore
+//sanjiv
+//jayavarsan
+
+
+
+
+const config = {
+    // Main Application URL
+    'websiteUrl' : 'https://imagecaptionai.com/',
+
+    // Application Name
+    'appName' : 'Qubots-Caption generator',
+
+    // Application window width and height
+    'width' : 1280,
+    'height' : 800,
+    'minWidth' : 1280,
+    'minHeight' : 800,
+
+    // Hide elements by ID
+    'hideElementsId' : [],
+
+    // Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
